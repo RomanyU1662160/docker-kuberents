1b11c6c00a1ffaaf3bde5d848f92f1e0da855014
Romany <romanyfayiez@hotmail.com>
2025-09-25 12:24:43 +0100

    feat: update feat config to accept the static url

--- fe-client/src/App.tsx
+++ fe-client/src/App.tsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
+const service1Url =
+  import.meta.env.VITE_SERVICE_1_URL_DEV || 'http://localhost:3000';
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+const getHealth = async () => {
+  const response = await fetch(`${service1Url}/health`);
+  const data = await response.json();
+  console.log('data:::>>>', data);
+  return data;
+};
+
+const getUsers = async (): Promise<User[]> => {
+  const response = await fetch(`${service1Url}/users`);
+  const data = await response.json();
+  return data.users;
+};
+console.log('service1Url', service1Url);
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([]);
+  const [isHealthy, setIsHealthy] = useState(false);
+
+  const fetchUsers = async () => {
+    const users = await getUsers();
+    console.log('users', users);
+    setUsers(users);
+  };
+
+  const fetchHealth = async () => {
+    const health = await getHealth();
+    console.log('health', health);
+    setIsHealthy(health.status === 'OK');
+  };
+
+  useEffect(() => {
+    fetchUsers();
+    fetchHealth();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='text-end p-4 bg-slate-200 font-mono'>
+        {' '}
+        Service health check:{' '}
+        <span>
+          {isHealthy ? (
+            <span className='text-green-600 font-bold animate-pulse'>
+              Healthy
+            </span>
+          ) : (
+            <span className='text-red-600 font-bold animate-pulse'>
+              Unhealthy
+            </span>
+          )}
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='container bg-slate-100 h-screen flex items-center justify-center flex-col gap-4'>
+        <div>
+          {users.length === 0 ? (
+            <div>Loading...</div>
+          ) : (
+            <div className='bg-white p-6 rounded shadow-md'>
+              <h1 className='text-2xl text-center text-red-900 font-bold mb-4'>
+                Users
+              </h1>
+              <ul>
+                {users.map((user: User) => (
+                  <li key={user.id} className='mb-2 w-full border-b pb-2'>
+                    <span className='font-semibold'>{user.name}</span> -{' '}
+                    <span className='text-gray-600'>{user.email}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- fe-client/vite.config.ts
+++ fe-client/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['.example.com', 'localhost', 'fe-client.example.com'],
+    hmr: {
+      clientPort: 80, // Use port 80 for HMR WebSocket connections
+      host: 'fe-client.example.com'
+    }
+  },
+});
+
+/*
+We need to set the host to '0.0.0.0' in the Vite config for Docker compatibility.
+because by default, Vite binds to 'localhost', which is not accessible from outside the container.
+this is necessary for the development server to be reachable when running inside a Docker container.
+
+*/
